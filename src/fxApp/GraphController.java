f9d94593aecc41ce1e45ee79d9df30007febaa68
Roshan Raj <roshan.raj@frogforce503.org>
2019-05-18 15:56:43 -0400

    added bar chart

--- src/fxApp/GraphController.java
+++ src/fxApp/GraphController.java
@@ -4,6 +4,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
 
 public class GraphController implements Initializable {
 
@@ -11,6 +12,7 @@ public class GraphController implements Initializable {
 	public void initialize(URL location, ResourceBundle resources) {
 	}
 
-	
+	@FXML
+	private BarChart<String, Number> barchart;
 	
 }
