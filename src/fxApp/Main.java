b090de53dc0b2223826d64b8bc7a44f7bde256a1
James Chen <jchen3652@gmail.com>
2019-05-18 14:20:06 -0400

    Formatted

--- src/fxApp/Main.java
+++ src/fxApp/Main.java
@@ -1,15 +1,11 @@
 package fxApp;
 
-import com.sun.javafx.application.LauncherImpl;
-
 import javafx.animation.PauseTransition;
 import javafx.application.Application;
-import javafx.application.Preloader;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
@@ -31,10 +27,9 @@ public class Main extends Application {
 			primaryStage.centerOnScreen();
 			primaryStage.setTitle("Exam Chief");
 			setPercentDone(0.95);
-//			primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("frogForce.png")));
+			// primaryStage.getIcons().add(new
+			// Image(getClass().getResourceAsStream("frogForce.png")));
 
-
-			
 			primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
 				public void handle(WindowEvent we) {
 				}
@@ -45,7 +40,6 @@ public class Main extends Application {
 
 			stage = primaryStage;
 
-
 			PauseTransition wait = new PauseTransition(Duration.hours(1.5));
 			wait.setOnFinished((e) -> {
 				primaryStage.close();
@@ -53,15 +47,16 @@ public class Main extends Application {
 			});
 			wait.play();
 
-//			previewJFrame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-//			previewJFrame.setSize(1080, 540);
-//			new Thread(new FieldClicker()).start();
-//			previewJFrame.setTitle("Graph With Field");
-//			previewJFrame.setLocationRelativeTo(null);
-//			previewJFrame.setIconImage(new javax.swing.ImageIcon(getClass().getResource("frogForce.png")).getImage());
-//			previewJFrame.setResizable(false);
-//			previewJFrame.addMouseListener(new FieldClicker());
-//			previewJFrame.addMouseMotionListener(new FieldClicker());
+			// previewJFrame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+			// previewJFrame.setSize(1080, 540);
+			// new Thread(new FieldClicker()).start();w
+			// previewJFrame.setTitle("Graph With Field");
+			// previewJFrame.setLocationRelativeTo(null);
+			// previewJFrame.setIconImage(new
+			// javax.swing.ImageIcon(getClass().getResource("frogForce.png")).getImage());
+			// previewJFrame.setResizable(false);
+			// previewJFrame.addMouseListener(new FieldClicker());
+			// previewJFrame.addMouseMotionListener(new FieldClicker());
 		} catch (Exception e) {
 		}
 	}
@@ -72,7 +67,7 @@ public class Main extends Application {
 		Parent root = FXMLLoader.load(getClass().getResource("MainDisplay.fxml"));
 		setPercentDone(0.75);
 		scene = new Scene(root);
-//		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		// scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 
 		KeyCode[] numberKeyCodes = { KeyCode.DIGIT0, KeyCode.DIGIT1, KeyCode.DIGIT2, KeyCode.DIGIT3, KeyCode.DIGIT4,
 				KeyCode.DIGIT5, KeyCode.DIGIT6, KeyCode.DIGIT7, KeyCode.DIGIT8, KeyCode.DIGIT9 };
@@ -92,12 +87,13 @@ public class Main extends Application {
 	}
 
 	public static void main(String[] args) throws Exception {
-//		LauncherImpl.launchApplication(Main.class, PreloaderPage.class, args);
+		// LauncherImpl.launchApplication(Main.class, PreloaderPage.class, args);
 		launch(args);
 	}
 
 	private void setPercentDone(double percent) {
-//		LauncherImpl.notifyPreloader(this, new Preloader.ProgressNotification(percent));
+		// LauncherImpl.notifyPreloader(this, new
+		// Preloader.ProgressNotification(percent));
 	}
 
 }
