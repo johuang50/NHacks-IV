05f5e05a6a6553ec355be4607f9f1d6c40e52293
Roshan Raj <roshan.raj@frogforce503.org>
2019-05-18 17:58:46 -0400

    home page labels

---
+++ src/fxApp/LGBTQContoller.java
@@ -0,0 +1,16 @@
+package fxApp;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class LGBTQContoller implements Initializable{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+	}
+	
+	
+
+}
